f38026ee40be3e45bae2d4f060675bd564859a90
Kartik Nayak <thisiskartiknayak@gmail.com>
2024-08-28 00:01:11 +0530

    feat: enhance HomePage with state management and custom widgets

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,13 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../common/custom_button.dart';
+import '../providers/home_provider.dart';
+import '../widgets/breach_status_indicator.dart';
+import '../widgets/password_length_slider.dart';
+import '../widgets/password_strength_widget.dart';
+import '../widgets/password_widget.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('HomePage'),
+    debugPrint('HomePage build is called');
+    final homeProvider = context.read<HomeProvider>();
+    homeProvider.initValues();
+
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('LOCK \$MITH'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+        child: Column(
+          children: [
+            const Spacer(),
+            const PasswordWidget(),
+            const SizedBox(height: 4.0),
+            const PasswordStrengthWidget(),
+            const SizedBox(height: 36.0),
+            const BreachStatusIndicator(),
+            const Spacer(),
+            const PasswordLengthSlider(),
+            CustomButton(
+              onTap: homeProvider.generateRandomPassword,
+              label: 'Random Password',
+            ),
+            CustomButton(
+              onTap: homeProvider.generateMemorablePassword,
+              label: 'Memorable Password',
+            ),
+          ],
+        ),
       ),
     );
   }
